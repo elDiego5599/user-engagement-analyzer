9e4de27d0d77440821fb6001f454292b583e030d
holajejej <98672873+elDiego5599@users.noreply.github.com>
2025-09-15 11:37:24 -0500

    Delete analyzer.py

--- analyzer.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def calculate_engagement_score(df):
-    # Correct formula: (likes + comments * 2 + shares * 3) / impressions
-    score = (df['likes'] + df['comments'] * 2 + df['shares'] * 3) / df['impressions']
-    return score.mean()
